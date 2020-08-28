3ffb3a244575c81fbad1e854b12788df2c99960b
Eihab <mastereihab@gmail.com>
2020-08-28 17:36:22 +0100

    added filter functionality

--- app.js
+++ app.js
@@ -42,6 +42,22 @@ function showLoading() {
     }, 1000);
 }
 
+function filterPosts(e) {
+    const term = e.target.value.toUpperCase();
+    const posts = document.querySelectorAll(".post")
+    posts.forEach(post => {
+        const title = post.querySelector(".post-title").innerText.toUpperCase()
+        const body = post.querySelector(".post-body").innerText.toUpperCase()
+
+        if (title.indexOf(term) > -1 || body.indexOf(term) > -1) {
+            post.style.display = "flex"            
+        } else {
+            post.style.display = "none"
+        }
+    })
+    
+}
+
 showPosts();
 
 // Event Listeners
@@ -52,3 +68,6 @@ addEventListener("scroll", () => {
         showLoading()
     }
 })
+
+
+filter.addEventListener("input", filterPosts)
